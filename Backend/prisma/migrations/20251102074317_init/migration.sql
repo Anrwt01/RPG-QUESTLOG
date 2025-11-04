89c54a086f00a15cf4a500cd90ed6c7faedbfe67
Anrwt01 <1anujrawat@gmail.com>
2025-11-04 16:59:48 +0530

    olla

---
+++ Backend/prisma/migrations/20251102074317_init/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "_id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "lastLogin" TIMESTAMP(3),
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("_id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
